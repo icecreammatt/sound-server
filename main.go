cc95e5a42d225bbfce3e6a1eff5b679dd56482da
Matt Carrier <icecreammatt@gmail.com>
2017-11-05 14:17:30 -0800

    Remove extra logging

--- main.go
+++ main.go
@@ -10,21 +10,15 @@ import (
 
 func playSoundFile(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
 
 	app := "mpg123"
 	arg1 := r.URL.Path
 	// s := []string{"/Users/matt/go/src/github.com/icecreammatt/sound-server", arg1}
+
 	s := []string{"/root", arg1}
 	musicCommand := strings.Join(s, "")
 	fmt.Println(musicCommand)
+
 	cmd := exec.Command(app, musicCommand)
 	stdout, err := cmd.Output()
 	if err != nil {
@@ -32,7 +26,7 @@ func playSoundFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	print(string(stdout))
-	fmt.Fprintf(w, "Playing Sound\n") // send data to client side
+	fmt.Fprintf(w, "Playing Sound\n")
 }
 
 func main() {
